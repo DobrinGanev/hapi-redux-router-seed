c28386f92014d9176dd112e5ca6d315f4440e3fa
Dobrin <dganev@chorstream.com>
2016-08-22 21:45:32 -0600

    added bunch of tests

--- src/components/__tests__/Home.spec.js
+++ src/components/__tests__/Home.spec.js
@@ -2,8 +2,8 @@ import React from 'react';
 import test from 'ava';
 import { shallow } from 'enzyme';
 const Home = () => (
-  <div>
-    <h2>Home</h2>
+  <div className="Home">
+    <h2 >Home</h2>
   </div>
   )
 test('has a .Home class name', t => {
